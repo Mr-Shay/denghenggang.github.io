b1377b5aed93a430be36972b9962474ae4f0c5d1
denghenggang <138355051+denghenggang@users.noreply.github.com>
2026-04-23 03:57:17 +0000

    Deploying to gh-pages from @ denghenggang/denghenggang.github.io@28244c7832189ab058a8842ce052caaf6f434ce1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Mr-Shay/denghenggang.github.io",
+    "data-repo": "denghenggang/denghenggang.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
